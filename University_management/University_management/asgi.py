9f09eb9aea9858d1e820930ac87fe203add94cf3
Dagim Zemene <Daguzemene@gmail.com>
2025-09-23 22:00:47 +0300

    created my account app

--- University_management/University_management/asgi.py
+++ University_management/University_management/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for university_managment project.
+ASGI config for University_management project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'university_managment.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'University_management.settings')
 
 application = get_asgi_application()
--- University_management/University_management/urls.py
+++ University_management/University_management/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for university_managment project.
+URL configuration for University_management project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
