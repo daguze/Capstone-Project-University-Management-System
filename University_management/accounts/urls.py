00900bf892f4447bcf67fd4eadb6224d850a2d64
Dagim Zemene <Daguzemene@gmail.com>
2025-09-24 22:24:10 +0300

    added the authentication method for registration login anfd logout

--- University_management/accounts/urls.py
+++ University_management/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from .views import RegisterView
+from django.contrib.auth.views import login, logout
+from . import views
 
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login'),
+    path('login/', views.user_login(template_name="registration/login.html"), name='login'),
     path('logout/', views.user_logout, name='logout'),
 ]
--- University_management/accounts/views.py
+++ University_management/accounts/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from .views import generic
+from django.urls import reverse_lazy
+
+
 # Create your views here.
 
+
+class RegisterView(generic.CreateView):
+    form_class = UserCreationForm
+    template_name = 'accounts/register.html'
+    success_url = reverse_lazy('login')
