38fec67349bd0853cdd7e87d9b19b89c6e421a4d
Dagim Zemene <Daguzemene@gmail.com>
2025-09-24 21:31:25 +0300

    added model data to my account app

--- University_management/accounts/models.py
+++ University_management/accounts/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+class Staff_user(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(null=True)
+    password = models.CharField(max_length=100)
+    date_joined = models.DateField(auto_now_add=True)
+
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}, {self.email}"
+    
+
+
+class Student_user(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(null=True)
+    password = models.CharField(max_length=100)
+    date_joined = models.DateField(auto_now_add=True)
+    department = models.CharField(max_length=100, null=True)
+
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}, {self.email} in {self.department}"
+    
+
+
+class Admin_user(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(null=True)
+    password = models.CharField(max_length=100)
+
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}, {self.email}"
+    
+
+
