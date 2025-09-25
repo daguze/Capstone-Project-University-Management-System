96db79eed63a6a4caca862b2662a53690373843e
Dagim Zemene <Daguzemene@gmail.com>
2025-09-25 15:12:16 +0300

    registration cousing errors to set only email not username

--- University_management/accounts/forms.py
+++ University_management/accounts/forms.py
@@ -1,14 +1,14 @@
-from django.contrib.auth.forms import UserCreationForm
+
 from django.contrib.auth.models import User
 from django import forms
 
-class RegisterForm(UserCreationForm):
+class RegisterForm(forms.ModelForm):
     email = forms.EmailField(required=True)
-    first_name = forms.CharField(max_length=30, required=True)
-    last_name = forms.CharField(max_length=30, required=True)
+    Full_name = forms.CharField(max_length=100, required=True)
+    password1 = forms.CharField(widget=forms.PasswordInput, required=True)
+    password2 = forms.CharField(widget=forms.PasswordInput, required=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
-    
-    
+        fields = ('email', 'Full_name', 'password1', 'password2')
+
--- University_management/accounts/models.py
+++ University_management/accounts/models.py
@@ -4,11 +4,15 @@ from django.contrib.auth.models import AbstractUser, Permission
 
 # Create your models here.
 class User(AbstractUser):
+    username = None
     Full_name = models.CharField(max_length=100)
     Entry_date = models.DateField(auto_now_add=True)
+    email = models.EmailField(unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['Full_name']
 
     def __str__(self):
-        return self.username
+        return self.Full_name
     
 
 class Staff_user(models.Model):
