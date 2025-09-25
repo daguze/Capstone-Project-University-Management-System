e6b4ce4e50784adf6c3ea15c3eea2cc789545d1b
Dagim Zemene <Daguzemene@gmail.com>
2025-09-25 14:42:21 +0300

    registration form

--- University_management/accounts/forms.py
+++ University_management/accounts/forms.py
@@ -6,3 +6,9 @@ class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+    
+    
