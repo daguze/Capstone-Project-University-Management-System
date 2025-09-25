5084e342bb859c2f7775ebabbddb26db4ee52c03
Dagim Zemene <Daguzemene@gmail.com>
2025-09-25 12:21:42 +0300

    trying to fix my previous codeas new due to compllexity

--- University_management/accounts/urls.py
+++ University_management/accounts/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path
-from .views import RegisterView, home
+from .views import home
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
+    #path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(template_name="accounts/login.html"), name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
-    path('', home.as_view(), name='home'),
+    path('', views.home, name='home'),
 ]
-router = DefaultRouter()
-router.register(r'staff', views.Staff_userViewSet, basename='staff')
-router.register(r'students', views.Student_userViewSet, basename='students')
-router.register(r'admins', views.Admin_userViewSet, basename='admins')
+#router = DefaultRouter()
+#router.register(r'staff', views.Staff_userViewSet, basename='staff')
+#router.register(r'students', views.Student_userViewSet, basename='students')
+#router.register(r'admins', views.Admin_userViewSet, basename='admins')
 
-urlpatterns += router.urls
+#urlpatterns += router.urls
--- University_management/accounts/views.py
+++ University_management/accounts/views.py
@@ -1,10 +1,17 @@
+from rest_framework import permissions, generics
+from rest_framework.response import Response
+from.models import Staff_user, Student_user
+from .serializers import StaffUserSerializer, StudentUserSerializer
+from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
-#from django.http import HttpResponse
-#from django.contrib.auth.forms import UserCreationForm
-#from django.views import generic
-#from django.urls import reverse_lazy
-#from django.contrib.auth.decorators import login_required
-# .models import Staff_user, Student_user
-#from rest_framework import viewsets, permissions
-#from rest_framework.decorators import action
-# Create your views here.
+def home(request):
+    return render(request, 'accounts/home.html')
+
+def login_user(request):
+    return render(request, 'accounts/login.html')
+
+def logout_user(request):
+    messages.success(request, "You have been logged out.")
+    return render(request, 'accounts/login.html')
