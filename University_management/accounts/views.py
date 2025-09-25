f7544748650fa09fd22b9c5e2685355f25867f50
Dagim Zemene <Daguzemene@gmail.com>
2025-09-25 13:20:27 +0300

    more work in the login views to to notautorising eroors

--- University_management/accounts/views.py
+++ University_management/accounts/views.py
@@ -2,24 +2,12 @@ from rest_framework import permissions, generics
 from rest_framework.response import Response
 from.models import Staff_user, Student_user
 from .serializers import StaffUserSerializer, StudentUserSerializer
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 
 def home(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user_check = authenticate(request, username=username, password=password)
-        if user_check is not None:
-            login(request, user_check)
-            messages.success(request, "You have successfully logged in.")
-            return render(request, 'accounts/home.html')
-        else:
-            messages.error(request, "Invalid username or password.")
-            return render(request, 'accounts/login.html')
-    else:
-        return render(request, 'accounts/home.html')
+    return render(request, 'accounts/home.html')
 
 def login_user(request):
     if request.method == 'POST':
@@ -29,15 +17,15 @@ def login_user(request):
         if user_check is not None:
             login(request, user_check)
             messages.success(request, "You have successfully logged in.")
-            return render(request, 'accounts/home.html')
+            return redirect('home')
         else:
             messages.error(request, "Invalid username or password.")
-            return render(request, 'accounts/login.html')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
 
 def logout_user(request):
     messages.success(request, "You have been logged out.")
-    return render(request, 'accounts/login.html')
+    return redirect('home')
   
