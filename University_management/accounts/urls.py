fd450d56dea97dd57bcf9d967a8fb42afe8def83
Dagim Zemene <Daguzemene@gmail.com>
2025-09-25 12:52:51 +0300

    working on login page

--- University_management/accounts/urls.py
+++ University_management/accounts/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from .views import home
+from .views import home, login_user
 urlpatterns = [
     path('',home, name='home'),
+    path('login/',login_user, name='login'),
    
 ]
 
