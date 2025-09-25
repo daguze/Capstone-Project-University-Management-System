133f1d0ee3e2f640464985c0f624500869b0c041
Dagim Zemene <Daguzemene@gmail.com>
2025-09-25 15:21:23 +0300

    fied registration form

--- University_management/accounts/forms.py
+++ University_management/accounts/forms.py
@@ -1,14 +1,17 @@
 
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django import forms
+
+from accounts.models import User
 
 class RegisterForm(forms.ModelForm):
     email = forms.EmailField(required=True)
-    Full_name = forms.CharField(max_length=100, required=True)
+    First_name = forms.CharField(max_length=100, required=True)
+    Last_name = forms.CharField(max_length=100, required=True)
     password1 = forms.CharField(widget=forms.PasswordInput, required=True)
     password2 = forms.CharField(widget=forms.PasswordInput, required=True)
 
     class Meta:
         model = User
-        fields = ('email', 'Full_name', 'password1', 'password2')
+        fields = ('email', 'First_name', 'Last_name', 'password1', 'password2')
 
