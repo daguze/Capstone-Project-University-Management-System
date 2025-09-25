6758559ae9188630684bd0c41420e9bcaf38fe12
Dagim Zemene <Daguzemene@gmail.com>
2025-09-25 12:28:40 +0300

    trying to fix servererror

--- University_management/University_management/settings.py
+++ University_management/University_management/settings.py
@@ -134,3 +134,4 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+AUTH_USER_MODEL = "accounts.User"
--- University_management/accounts/urls.py
+++ University_management/accounts/urls.py
@@ -1,18 +1,10 @@
+
 from django.urls import path
 from .views import home
-from django.contrib.auth.views import LoginView, LogoutView
-from . import views
-from rest_framework.routers import DefaultRouter
-
 urlpatterns = [
-    #path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(template_name="accounts/login.html"), name='login'),
-    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
-    path('', views.home, name='home'),
+    path('',home, name='home'),
+   
 ]
-#router = DefaultRouter()
-#router.register(r'staff', views.Staff_userViewSet, basename='staff')
-#router.register(r'students', views.Student_userViewSet, basename='students')
-#router.register(r'admins', views.Admin_userViewSet, basename='admins')
 
-#urlpatterns += router.urls
+
+    
