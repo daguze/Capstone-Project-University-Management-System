435ae099a5341dd3768305f2751a8ca85d13fd14
Dagim Zemene <Daguzemene@gmail.com>
2025-09-25 15:53:08 +0300

    correcting abstract model

--- University_management/accounts/models.py
+++ University_management/accounts/models.py
@@ -1,16 +1,15 @@
-from email.headerregistry import Group
+
 from django.db import models
-from django.contrib.auth.models import AbstractUser, Permission
+from django.contrib.auth.models import AbstractUser, Permission, Group
 
 # Create your models here.
 class User(AbstractUser):
     Full_name = models.CharField(max_length=100)
     Entry_date = models.DateField(auto_now_add=True)
-    email = models.EmailField(unique=True)
     
 
     def __str__(self):
-        return self.Full_name
+        return self.Username
     
 
 class Staff_user(models.Model):
