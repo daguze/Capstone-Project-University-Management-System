fb0762fb9601493c83354271b96b3520f930d006
Dagim Zemene <Daguzemene@gmail.com>
2025-09-30 21:33:08 +0300

    integrated my course app and grade into my accounts tab

--- University_management/Courses/admin.py
+++ University_management/Courses/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Course, Grade
 # Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("code", "title", "department")
+    search_fields = ("code", "title", "department", "instructor__user__full_name")
+    
+
+
+@admin.register(Grade)
+class GradeAdmin(admin.ModelAdmin):
+    list_display = ("course", "score")
+    search_fields = ("student__user__full_name", "course__code", "course__title")
+    list_select_related = ("student__user", "course")
